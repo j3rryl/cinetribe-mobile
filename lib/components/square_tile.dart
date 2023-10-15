e1db77b4ceba4fbe17aa8fec9d345dc56eaaf153
j3rryl <jeremymunroe0@gmail.com>
2023-10-15 10:11:28 +0300

    login page

---
+++ lib/components/square_tile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SquareTile extends StatelessWidget {
+  final String imagePath;
+  final Color imageColor;
+  const SquareTile({
+    super.key,
+    required this.imagePath,
+    required this.imageColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.circular(16),
+            color: imageColor),
+        child: Image.asset(imagePath, height: 40));
+  }
+}
---
+++ lib/views/auth/login_view.dart
@@ -0,0 +1,183 @@
+import 'package:cinetribe/components/my_button.dart';
+import 'package:cinetribe/components/my_textfield.dart';
+import 'package:cinetribe/components/square_tile.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  // final VoidCallback showingRegisterPage;
+
+  // LoginPage({Key? key, required this.showingRegisterPage}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
+  void signIn() async {
+    //loading
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const Center(child: CircularProgressIndicator());
+        });
+    //sign In
+    // try {
+    //   await FirebaseAuth.instance.signInWithEmailAndPassword(
+    //       email: emailController.text, password: passwordController.text);
+    //   Navigator.pop(context);
+    // } on FirebaseAuthException catch (e) {
+    //   Navigator.pop(context);
+
+    //   if (e.code == 'user-not-found') {
+    //     wrongEmail();
+    //   } else if (e.code == 'wrong-password') {
+    //     wrongPassword();
+    //   }
+    // }
+  }
+
+  // void wrongEmail() {
+  //   showDialog(
+  //       context: context,
+  //       builder: (context) {
+  //         return const AlertDialog(title: Text('Incorrect email'));
+  //       });
+  // }
+
+  // void wrongPassword() {
+  //   showDialog(
+  //       context: context,
+  //       builder: (context) {
+  //         return const AlertDialog(title: Text('Incorrect password'));
+  //       });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+            child: SingleChildScrollView(
+                child: Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+              Container(
+                height: 200,
+                width: 400,
+                // child: Lottie.asset('assets/animations/animated_lock.json'),
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.left,
+              ),
+              const SizedBox(height: 25),
+              MyTextField(
+                controller: emailController,
+                hintText: "Email",
+                obscureText: false,
+              ),
+              const SizedBox(height: 10),
+              MyTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true,
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('Forgot Password? ',
+                        style: TextStyle(color: Colors.grey[600]))
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              MyButton(
+                onTap: signIn,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          'Or continue with',
+                          style: TextStyle(color: Colors.grey[700]),
+                        )),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquareTile(
+                      imagePath: 'assets/images/google.png',
+                      imageColor: Colors.grey[200]!),
+                  const SizedBox(width: 10),
+                  const SquareTile(
+                    imagePath: 'assets/images/github.png',
+                    imageColor: Colors.black,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  GestureDetector(
+                    // onTap: widget.showingRegisterPage,
+                    child: const Text(
+                      'Register Now',
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 50),
+            ])))));
+  }
+}
