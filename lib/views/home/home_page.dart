3b79ac328c6bb56cd000cf003585431dab5a3a0d
j3rryl <jeremymunroe0@gmail.com>
2023-10-19 15:13:20 +0300

    ...

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,11 +9,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void logout() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
+      backgroundColor: Colors.grey.shade200,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.transparent,
+            // leading: Icon(Icons.menu),
+            actions: [
+              const Icon(Icons.search, color: Colors.black),
+              const SizedBox(width: 8),
+              IconButton(
+                icon: const Icon(Icons.logout),
+                color: Colors.black,
+                onPressed: logout,
+              ),
+              const SizedBox(width: 14),
+            ],
+            expandedHeight: 225,
+            floating: true,
+            pinned: false,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: Colors.pink,
+              ),
+              title: const Text("C I N E T R I B E"),
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: [],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
